f2eb488dd1ef6cb5dda77655d7daf1566711141a
Mathis Durand <mathis.durand@hotmail.com>
2025-04-03 20:29:42 +0200

    Modif du lien

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 CLIENT_ID = "152701"
 CLIENT_SECRET = "f9994d3d0eac0d314a1ee9c94ccb2dd674debb5d"
-REDIRECT_URI = "http://localhost:5000/callback"  # Remplacez par votre URL de redirection
+REDIRECT_URI = "http://localhost:5000/callback"
 
 ATHLETES_FILE = "athletes.json"
 
