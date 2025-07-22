413fa7978cea8d21d9e46a7206cd143a72b1b6f5
Mathis Durand <mathis.durand@hotmail.com>
2025-07-22 22:31:47 +0200

    update for render

--- server.py
+++ server.py
@@ -100,6 +100,6 @@ def callback():
 
 
 # to execute the code in local
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
